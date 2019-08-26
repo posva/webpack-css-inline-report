ceadd201dbd7da20ece7c4532e51d4fd1133aa8e
Eduardo San Martin Morote <posva13@gmail.com>
2019-08-26 11:08:36 +0200

    more

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HTMLInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin')
   .default
-const path = require('path')
 
 module.exports = () => {
   /** @type {import('webpack').Configuration} */
   const config = {
-    entry: path.resolve(__dirname, './index.js'),
     mode: 'production',
 
     plugins: [
